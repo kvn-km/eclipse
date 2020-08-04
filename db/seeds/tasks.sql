ecbbe7d09cc3863694fdda99602df68142f6db10
Victor Liang <victor.zw.liang@gmail.com>
2020-08-04 09:29:22 -0400

    Made express server for database api

---
+++ db/seeds/tasks.sql
@@ -0,0 +1,2 @@
+INSERT INTO tasks (id, name, completion_time_in_seconds, type, xp_points, times_to_complete, description)
+VALUES (1, 'plank', 3000, 'exercise', 50, NULL, 'An exercise training the bodys core.');
---
+++ db/seeds/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, name, password, email, username, date_created, experience_points, level) VALUES (1, 'Test','test123', 'test@test.com', 'test123', '2020-08-03', 0, 1);
+
+INSERT INTO users (id, name, password, email, username, date_created, experience_points, level) VALUES (2, 'Kevin','kkim', 'kkim@test.com', 'kkim', '2020-08-02', 3000, 25);
+
+INSERT INTO users (id, name, password, email, username, date_created, experience_points, level) VALUES (3, 'Victor','vliang', 'vliang@test.com', 'vilang', '2020-08-02', 5000, 30);
