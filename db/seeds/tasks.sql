ea1b44e001cbd5e9f19fb645110b07155baafe8e
Kevin Kim <kevinfkim@gmail.com>
2020-08-05 13:58:45 -0400

    refactor seeds: user, and add some seeds: tasks.

--- db/seeds/tasks.sql
+++ db/seeds/tasks.sql
@@ -1,2 +1,11 @@
 INSERT INTO tasks (id, name, completion_time_in_seconds, type, xp_points, times_to_complete, description)
-VALUES (1, 'plank', 3000, 'exercise', 50, NULL, 'An exercise training the bodys core.');
+VALUES  (1, 'plank', 3000, 'exercise', 50, 3, 'An exercise training the bodys core.'),
+        (2, 'jumping jacks', 3000, 'exercise', 50, 3, 'An exercise training the heart, lungs, and muscles at the same time'),
+        (3, 'squats', 3000, 'exercise', 50, 3, 'A strength exercise in which the trainee lowers their hips from a standing position and then stands back up.'),
+        (4, 'push ups', 3000, 'exercise', 50, 3, 'A common calisthenics exercise beginning from the prone position, and raising and lowering the body using the arms.'),
+        (5, 'stretches', 3000, 'exercise', 50, 6, 'Stretching keeps the muscles flexible, strong, and healthy, and we need that flexibility to maintain a range of motion in the joints.'),
+        (6, 'sit ups', 3000, 'exercise', 50, 3, 'Sit ups are an excellent core muscle exercise that target your abdominal muscles, hip flexors, and lower back.'),
+        (7, 'tree pose', 3000, 'exercise', 50, 2, 'Stretches the thighs, groins, torso, and shoulders, and builds strength in the ankles and calves, and tones the abdominal muscles.'),
+        (8, 'stillness', 5000, 'exercise', 50, 6, 'Stillness is a conscious choice to set aside a period of time and let the body, mind and soul rest.'),
+        (9, 'drink water', 3000, 'diet', 50, 5, 'It\''s good to hydrate.'),
+        ;
--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -1,5 +1,4 @@
-INSERT INTO users (id, name, password, email, username, date_created, experience_points, level) VALUES (1, 'Test','test123', 'test@test.com', 'test123', '2020-08-03', 0, 1);
-
-INSERT INTO users (id, name, password, email, username, date_created, experience_points, level) VALUES (2, 'Kevin','kkim', 'kkim@test.com', 'kkim', '2020-08-02', 3000, 25);
-
-INSERT INTO users (id, name, password, email, username, date_created, experience_points, level) VALUES (3, 'Victor','vliang', 'vliang@test.com', 'vilang', '2020-08-02', 5000, 30);
+INSERT INTO users (id, name, password, phone, username, date_created, experience_points, level) 
+VALUES  (1, 'Test','test123', '1231231234', 'test123', '2020-08-03', 0, 1),
+        (2, 'Kevin','kkim', '1231231234', 'kkim', '2020-08-02', 3000, 25),
+        (3, 'Victor','vliang', '1231231234', 'vilang', '2020-08-02', 5000, 30);
