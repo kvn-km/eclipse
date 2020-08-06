fa435c49ac1167a7de3672820db8108f52607adc
Victor Liang <victor.zw.liang@gmail.com>
2020-08-05 22:16:35 -0400

    Added comment for next step

--- db/index.js
+++ db/index.js
@@ -8,17 +8,7 @@ const pool = new Pool({
   port: 5432
 });
 
-// const getUsers = (request, response) => {
-//   console.log(request.params);
-//   console.log(request.query);
-//   pool.query('SELECT * FROM users;', (error, results) => {
-//     if (error) {
-//       throw error;
-//     }
-//     response.status(200).json(results.rows);
-//   });
-// };
-
+//ISSUE: IF USERS PASSWORD OF ANOTHER USER, THEY CAN STILL GET IN
 const getUser = (request, response) => {
   console.log(request.query);
   pool.query('SELECT * FROM users WHERE UPPER(name) = $1 OR password = $2;', [request.query.name, request.query.password], (error, results) => {
