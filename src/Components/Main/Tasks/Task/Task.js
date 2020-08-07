1198f4c36db29424de310a70dc661b50995b3345
Kevin Kim <kevinfkim@gmail.com>
2020-08-07 02:09:10 -0400

    adjustment to conflict file.

--- src/Components/Main/Tasks/Task/Task.js
+++ src/Components/Main/Tasks/Task/Task.js
@@ -18,29 +18,17 @@ function refreshPage() {
 function Task(props) {
 
   return (
-<<<<<<< HEAD
-
-    <section className="task main">
-      <div className="task-title">{`${mockData.taskTitle}`}</div>
-      <button type="button" onClick={init}>Start</button>
-      <div className="canvas-canvas" id><canvas id="canvas">Hello</canvas></div>
-      <div id="label-container"></div>
-      <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/pose@0.8/dist/teachablemachine-pose.min.js"></script>
-      <button type="button" onClick={refreshPage}>Refresh</button>
-    </section >
-=======
     <FadeIn>
       <section className="task main">
         <div className="task-title">{`${mockData.taskTitle}`}</div>
         <button type="button" onClick={init}>Start</button>
-        <div className="canvas-canvas"><canvas id="canvas"></canvas></div>
+        <div className="canvas-canvas" id><canvas id="canvas">Hello</canvas></div>
         <div id="label-container"></div>
         <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/pose@0.8/dist/teachablemachine-pose.min.js"></script>
+        <button type="button" onClick={refreshPage}>Refresh</button>
       </section >
     </FadeIn>
->>>>>>> data/connect-data-to-ui
   );
 }
 
