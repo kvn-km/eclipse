8bb4e407184e68244ccdd9e9d33e886b547cd892
Kevin Kim <kevinfkim@gmail.com>
2020-08-07 19:46:39 -0400

    switch to branch

--- src/Components/Main/Profile.js
+++ src/Components/Main/Profile.js
@@ -6,104 +6,57 @@ import "./main.scss";
 import "./GoalsAchievs/achievs.scss";
 import Achievs from "./GoalsAchievs/Achievs";
 
-const mockData = [
-  {
-    id: 1,
-    taskTitle: "Task Title 1",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 2,
-    taskTitle: "Task Title 2",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 3,
-    taskTitle: "Task Title 3",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 4,
-    taskTitle: "Task Title 4",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 5,
-    taskTitle: "Task Title 5",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 6,
-    taskTitle: "Task Title 6",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 7,
-    taskTitle: "Task Title 7",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 8,
-    taskTitle: "Task Title 8",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 9,
-    taskTitle: "Task Title 9",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 10,
-    taskTitle: "Task Title 10",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 11,
-    taskTitle: "Task Title 11",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-  {
-    id: 12,
-    taskTitle: "Task Title 12",
-    taskProgress: 100,
-    description: "GREAT JOB!"
-  },
-];
-
 function Profile(props) {
-  let [user, setUser] = useState({ info: null, levelInfo: null });
+  let [user, setUser] = useState({
+    info: null,
+    levelInfo: null,
+    usersAchievs: null,
+    allAchievs: []
+  });
 
   console.log("TEMPVAR", user);
 
-  // let user_id = "";
-  // if (props.match.url.includes("/profile")) {
-  //   user_id = props.match.url.slice(6, 7);
-  // }
+  let location = props.location.pathname;
 
   useEffect(() => {
     let mounted = true;
     const currentUser = () => {
-      Promise.all([axios.get('/api/user/current', { params: { id: props.match.url.slice(6, 7) } }), axios.get('/api/levels')])
+      Promise.all([
+        axios.get('/api/user/current', { params: { id: location.slice(6, 7) } }),
+        axios.get('/api/levels'),
+        axios.get('/api/tasks/user', { params: { id: location.slice(6, 7) } }),
+        axios.get('/api/tasks/side')])
         .then((all) => {
           let level = all[1].data[all[0].data.level - 1];
-          mounted && setUser((prev) => ({ ...prev, info: all[0].data, levelInfo: level.xp }));
+          let allUserTasks = all[2].data;
+          let allTasks = all[3].data;
+          mounted && setUser((...prev) => ({
+            info: all[0].data,
+            levelInfo: level.xp,
+            tasks: allUserTasks,
+            allTasks: allTasks
+          }));
         });
     };
     currentUser();
     return () => { mounted = false; };
-  }, [props.match.url]);
+  }, [location]);
 
+  // const tasks = user.allTasks.map((task, i) => {
+  //   const userTask = user.tasks[i + 8];
+  //   return (
+  //     <TaskButton
+  //       key={task.id}
+  //       id={task.id}
+  //       link={"/user/2/task"}
+  //       taskTitle={task.name}
+  //       progress={userTask.progress}
+  //       taskCompletionAmount={`${userTask.progress}%`}
+  //       description={task.description}
+  //     />
+
+  //   );
+  // });
 
   const tasks = mockData.map((task) => {
     return (
